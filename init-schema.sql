5f8fefad379e15ecabc318ed6a0da0800f5290de
Tim Sanz <tim@sanz.berlin>
2020-06-14 13:44:47 +0200

    Bugfix SQL
    Fixed invalid SQL type name in init script

--- init-schema.sql
+++ init-schema.sql
@@ -1,16 +1,16 @@
-create type einstrahlung-enum as enum ('flachlage', 'sonnenhang', 'schattenhang');
+create type einstrahlung_enum as enum ('flachlage', 'sonnenhang', 'schattenhang');
 
 create table lagen (
 	nummer			integer primary key,
 	höhe			integer,
 	hangneigung		integer,
-	einstrahlung	einstrahlung-enum
+	einstrahlung	einstrahlung_enum
 );
 
-create type temperatur-enum as enum ('kalt', 'mittel', 'warm');
+create type temperatur_enum as enum ('kalt', 'mittel', 'warm');
 
 create table klimas (
 	nummer			integer primary key,
-	temperatur		temperatur-enum,
+	temperatur		temperatur_enum,
 	niederschlag	integer
 );
