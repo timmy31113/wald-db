d13e7a89547c961a562c70933deb83afdad409c0
Tim Sanz <tim@sanz.berlin>
2020-06-14 17:38:52 +0200

    Added example data
    New SQL script example-data.sql includes some example data.

---
+++ example-data.sql
@@ -0,0 +1,30 @@
+insert into lagen (nummer, höhe, hangneigung, einstrahlung)
+	values (1, 30, 0, 'flachlage');
+insert into lagen (nummer, höhe, hangneigung, einstrahlung)
+	values (2, 150, 20, 'sonnenhang');
+insert into lagen (nummer, höhe, hangneigung, einstrahlung)
+	values (3, 80, 30, 'schattenhang');
+insert into lagen (nummer, höhe, hangneigung, einstrahlung)
+	values (4, 500, 5, 'flachlage');
+
+insert into klimas (nummer, temperatur, niederschlag)
+	values (1, 'kalt', 700);
+insert into klimas (nummer, temperatur, niederschlag)
+	values (2, 'warm', 850);
+insert into klimas (nummer, temperatur, niederschlag)
+	values (3, 'mittel', 600);
+insert into klimas (nummer, temperatur, niederschlag)
+	values (4, 'warm', 1800);
+insert into klimas (nummer, temperatur, niederschlag)
+	values (5, 'kalt', 1000);
+
+insert into böden (nummer, art, feuchte)
+	values (1, 'sand', 'trocken');
+insert into böden (nummer, art, feuchte)
+	values (2, 'schluff', 'frisch');
+insert into böden (nummer, art, feuchte)
+	values (3, 'lehm', 'feucht');
+insert into böden (nummer, art, feuchte)
+	values (4, 'ton', 'feucht');
+
+
