010ed76ea08c502d474fe81df747a8fe07fa026c
Tim Sanz <tim@sanz.berlin>
2020-06-14 13:56:14 +0200

    Expanded init SQL
    Now contains new table böden.
    All occurences of integer primary key have been converted to Postgres
    serial type.

--- init-schema.sql
+++ init-schema.sql
@@ -1,7 +1,7 @@
 create type einstrahlung_enum as enum ('flachlage', 'sonnenhang', 'schattenhang');
 
 create table lagen (
-	nummer			integer primary key,
+	nummer			serial primary key,
 	höhe			integer,
 	hangneigung		integer,
 	einstrahlung	einstrahlung_enum
@@ -10,7 +10,17 @@ create table lagen (
 create type temperatur_enum as enum ('kalt', 'mittel', 'warm');
 
 create table klimas (
-	nummer			integer primary key,
+	nummer			serial primary key,
 	temperatur		temperatur_enum,
 	niederschlag	integer
+);
+
+create type bodenart as enum ('sand', 'schluff', 'lehm', 'ton');
+
+create type bodenfeuchte as enum ('trocken', 'frisch', 'feucht', 'nass', 'sehr nass');
+
+create table böden (
+	nummer	serial primary key,
+	art		bodenart,
+	feuchte	bodenfeuchte
 );
